4d1bc73d1ecf6e262a48501e61f57bfcef11b208
Luiz Felipe <102871467+LuizFelipe321@users.noreply.github.com>
2026-08-17 21:23:59 -0300

    Create atividade3.c

---
+++ atividade3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define altura 5
+
+int main(){
+
+    int base, altura, perimetro;
+
+    printf("Escreva o valor da base:\n");
+    scanf("%d", &base);
+
+    perimetro = base * altura;
+
+    printf("Resultado do perimetro do retangulo: %d", perimetro);
+
+    return 0;
+}
